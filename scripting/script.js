d70adf6a2bb4bf736be68dbc069c7210636b6da1
Sophyy <Sofiyullahj@gmail.com>
2018-12-21 14:46:09 +0100

    added fadeIn method to the site banner

--- scripting/script.js
+++ scripting/script.js
@@ -2,4 +2,7 @@ $(document).ready(() => {
     $(".burger-nav").on("click", ()=> {
         $("header nav ul").toggleClass("open");
     });
+
+    $('.banner').hide();
+    $('.banner').fadeIn(2000);
 });
